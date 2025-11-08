75fb2309a4d728b12b55b87afa266639d67348a0
jinurajtvpm <jinurajtvpm@gmail.com>
2025-11-08 12:53:53 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'mess-management-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Add other assets you want to cache
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
